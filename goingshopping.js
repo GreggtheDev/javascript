c5a7ca070d39a327ea2034ae95823c5649245936
geepersnc <geepersnc@gmail.com>
2024-06-11 12:36:20 -0400

    added froot loops to shopping list and changed coffee to fair trade coffee

---
+++ goingshopping.js
@@ -0,0 +1,23 @@
+// Create an array called shoppingList with the following items: 'pop tarts', 'ramen noodles', 'chips', 'salsa', and 'coffee'.
+let shoppingList = ['pop tarts', 'ramen noodles', 'chips', 'salsa', 'coffee'];
+
+// Add 'fruit loops' to the list
+shoppingList.push('froot loops');
+
+// Update 'coffee' to 'fair trade coffee'
+let index = shoppingList.indexOf('coffee');
+if (index !== -1) {
+  shoppingList[index] = 'fair trade coffee';
+}
+
+// TODO: Implement your shopping functions
+
+// TODO: Test your shopping functions
+
+// TODO: Call your shopping functions
+
+// TODO: Print the results
+
+// TODO: Add any additional code or notes
+
+// Happy shopping!
